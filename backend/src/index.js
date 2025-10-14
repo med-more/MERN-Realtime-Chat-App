002babf7cc78635498e1a564f1f8cdc7d643e907
med-more <166568621+med-more@users.noreply.github.com>
2025-10-14 23:24:13 +0000

    configurate the index.js to read and handle the cookies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import {connectDB} from "./lib/db.js"
 
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
+
 dotenv.config()
 const PORT = process.env.PORT
 app.use("/api/auth", authRoutes);
