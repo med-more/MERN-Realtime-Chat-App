77374fe3060d20310ed5ce24d78a22f9c5b6346a
med-more <166568621+med-more@users.noreply.github.com>
2025-10-14 23:06:16 +0000

    implement the update profile route

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.put("/update-profile", protectRoute, updateProfile);
 
 export default router;
