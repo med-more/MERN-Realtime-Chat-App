97531d801780ad01cc117ffe5ea50250a5fdf0a5
med-more <166568621+med-more@users.noreply.github.com>
2025-10-14 16:13:38 +0000

    implement user signup with JWT token generation and secure cookie handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,47 @@
-export const signup = (req, res)=>{
-    
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+
+export const signup = async (req, res)=>{
+const { fullName, email, password } = req.body;
+    try {
+        if(!fullName || !email || !password){
+            return res.status(400).json({ message: "All field are required" });
+        }
+        if(password.length<6){
+            return res.status(400).json({ message: "password must be at least 6 characters" });
+        }
+        const user = await User.findOne({email})
+
+        if(user) return res.status(400).json({ message: "user already exist" });
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        });
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic
+            });
+        } else {
+            res.status(400).json({message: "invalide user data"});
+        }
+    } catch (error) {
+        console.log("error in the signup controller");
+        res.status(500).json({ message: "Internal server error" });
+    }
 };
 
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import {connectDB} from "./lib/db.js"
 
 
 const app = express();
+app.use(express.json());
 dotenv.config()
 const PORT = process.env.PORT
 app.use("/api/auth", authRoutes);
