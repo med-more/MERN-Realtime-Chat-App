5f07b02209680485c94bba415a6c9e038a47c9d6
med-more <166568621+med-more@users.noreply.github.com>
2025-10-15 01:39:58 +0000

    add checkAuth controller to return authenticated user data

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -99,4 +99,13 @@ export const updateProfile = async (req, res) =>{
         console.log("Error in update Profile", error);
         res.status(500).json({message: "internal server error"});
     }
-}
+};
+
+export const checkAuth = (req, res) =>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
+        res.status(500).json({message: "internal server error"});
+    }
+}; 
