d35eb8e53293ed27d7d8e53c178c38076499bc14
med-more <166568621+med-more@users.noreply.github.com>
2025-10-15 12:59:59 +0000

    add getMessages controller to retrieve chat history between two users

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model";
 
 export const getUsersForSideBar = async(req, res) =>{
     try {
@@ -7,6 +8,26 @@ export const getUsersForSideBar = async(req, res) =>{
         res.status(200).json(filtredUsers);
     } catch (error) {
         console.log("Error in getUsersForSideBar", error.message);
+        res.status(500).json({message: "internal server error"});
+    }
+};
+
+
+export const getMessages = async(req, res) =>{
+    try {
+        const {id: userToChatId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId:userToChatId},
+                {senderId: userToChatId, receiverId:myId},
+            ],
+        });
+
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessages controller", error);
         res.status(500).json({message: "internal server error"});
     }
 };
