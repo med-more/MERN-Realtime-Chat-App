6493d7acd86585cb424d5558fe62fab1cfe5bc51
med-more <166568621+med-more@users.noreply.github.com>
2025-10-15 02:36:57 +0000

    add getUsersForSideBar controller to fetch all users except the logged-in user

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,12 @@
+import User from "../models/user.model.js";
+
 export const getUsersForSideBar = async(req, res) =>{
-    
-}
+    try {
+        const loggedInUserId = req.user._id; 
+        const filtredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password");
+        res.status(200).json(filtredUsers);
+    } catch (error) {
+        console.log("Error in getUsersForSideBar", error.message);
+        res.status(500).json({message: "internal server error"});
+    }
+};
