9f16c11f1647d7d4b722b7527a864ea10f32f2cf
med-more <166568621+med-more@users.noreply.github.com>
2025-10-16 00:43:21 +0000

    configure the app.jsx and main.jsx with react-dom-router && create the components and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/signup" element={<SignUpPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
+        <Route path="/settings" element={<SettingsPage />}/>
+        <Route path="/profile" element={<ProfilePage />}/>
+      </Routes>
     </div>
   )
 }
