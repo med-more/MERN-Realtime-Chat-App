2c1b88c2e4c6ff71fd20a7af0475e3c5b1b25b51
med-more <166568621+med-more@users.noreply.github.com>
2025-10-16 15:27:52 +0000

    add responsive layout with logo and header section

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { useAuthStore } from "../store/useAuthStore";
+import { MessageSquare } from "lucide-react";
 
 const SignUpPage = () => {
 
@@ -9,9 +11,39 @@ const SignUpPage = () => {
         password: "",
     });
 
-    
+    const { signup, isSigningUp } = useAuthStore();
+
+    const validateForm = () =>{
+
+    };
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        
+    };
+
+
   return (
-    <div>SignUpPage</div>
+    <div className="min-h-screen grid lg:grid-cols-2">
+        {/* left side */}
+        <div className="flex flex-col justify-center items-center p-6 sm:p-12">
+
+            <div className="w-full max-w-md space-y-8">
+                {/* Logo */}
+                <div className="text-center mb-8">
+                    <div className="flex flex-col items-center gap-2 group">
+                        <div className="size-12 rounded-xl bg-primary/10 flex items-center justify-center group-hover:bg-primary/20 transition-colors">
+                            <MessageSquare className="size-6 text-primary" />
+                        </div>
+                        <h1 className="text-2xl font-bold mt-2">Create Account</h1>
+                        <p className="text-base-content/60">Get Started With Your Free Account</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
   )
 }
 
