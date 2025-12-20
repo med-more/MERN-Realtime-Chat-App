38aabdea9c9b95a970434d3a6e2f7b8f31cd0ccd
med-more <166568621+med-more@users.noreply.github.com>
2025-12-20 15:48:35 +0000

    implement profile picture upload with base64 preview

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { useAuthStore } from "../store/useAuthStore"
 import {Camera, User, Mail} from "lucide-react"
 
 const ProfilePage = () => {
   const {authUser, isUpdatingProfile, updateProfile} = useAuthStore();
+  const [selectedImg, setSelectedImg] = useState(null);
+
 
   const handleImageUpload = async (e) =>{
-      
-  }
+      const file = e.target.files[0];
+      if (!file) {
+        return;
+      }
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.onload = async () =>{
+        const base64Image = reader.result;
+        setSelectedImg(base64Image);
+        await updateProfile({ profilePic: base64Image })
+      }
+  };
   return (
     <div className="h-screen pt-20">
       <div className="max-w-2xl mx-auto p-4 py-8">
